9ac69e11c9601b603f90fa58e52e6c3e8e4e3813
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-20 09:44:47 -0700

    More Main refactoring, modified displayBalance().  Input starter code for withdrawMoney() and addMoney().

--- UnitTesting/UnitTesting/Program.cs
+++ UnitTesting/UnitTesting/Program.cs
@@ -8,9 +8,11 @@ namespace UnitTesting
         {
             bool execute = true;
             string userSelection;
-            decimal balance = 2000;
+            decimal balance = 2000.32659M;
 
             Console.WriteLine("Welcome to Bank of CF!");
+            Console.WriteLine();
+            displayBalance(balance);
             Console.WriteLine();
             Console.WriteLine("Please make a selection from the following commands:");
             userSelection = selectionPrompt();
@@ -20,6 +22,7 @@ namespace UnitTesting
 
                 if(userSelection.ToUpper() == "1" || userSelection.ToUpper() == "VIEW BALANCE" || userSelection.ToUpper() == "VIEW" || userSelection.ToUpper() == "BALANCE")
                 {
+                    Console.Clear();
                     displayBalance(balance);
                     Console.WriteLine("Please make another selection:");
                     userSelection = selectionPrompt();
@@ -27,7 +30,9 @@ namespace UnitTesting
 
                 else if (userSelection.ToUpper() == "2" || userSelection.ToUpper() == "WITHDRAW MONEY" || userSelection.ToUpper() == "WITHDRAW" || userSelection.ToUpper() == "WITHDRAWAL")
                 {
-                    balance = withdrawMoney();
+                    decimal value = 0;
+                    balance = withdrawMoney(balance, value);
+                    Console.Clear();
                     displayBalance(balance);
                     Console.WriteLine("Please make another selection:");
                     userSelection = selectionPrompt();
@@ -35,13 +40,15 @@ namespace UnitTesting
 
                 else if (userSelection.ToUpper() == "3" || userSelection.ToUpper() == "ADD MONEY" || userSelection.ToUpper() == "ADD" || userSelection.ToUpper() == "DEPOSIT")
                 {
-                    balance = addMoney();
+                    decimal value = 0;
+                    balance = addMoney(balance, value);
+                    Console.Clear();
                     displayBalance(balance);
                     Console.WriteLine("Please make another selection:");
                     userSelection = selectionPrompt();
                 }
 
-                else if (userSelection.ToUpper() == "1" || userSelection.ToUpper() == "VIEW BALANCE" || userSelection.ToUpper() == "VIEW" || userSelection.ToUpper() == "BALANCE")
+                else if (userSelection.ToUpper() == "4" || userSelection.ToUpper() == "EXIT" || userSelection.ToUpper() == "QUIT" || userSelection.ToUpper() == "CLOSE" || userSelection.ToUpper() == "END")
                 {
                     execute = false;
                 }
@@ -50,6 +57,7 @@ namespace UnitTesting
                 {
                     Console.WriteLine("Please make a valid selection.");
                     Console.WriteLine();
+                    displayBalance(balance);
                     userSelection = selectionPrompt();
                 }
 
@@ -64,25 +72,24 @@ namespace UnitTesting
             Console.WriteLine("2. Withdraw Money");
             Console.WriteLine("3. Add Money");
             Console.WriteLine("4. Exit");
-            Console.Write("Input selection: ");
+            Console.Write("Please make a selection: ");
             return Console.ReadLine();
         }
 
         static void displayBalance(decimal balance)
         {
-            Console.WriteLine();
-            Console.WriteLine($"Your new balance is: {balance.ToString("C")}");
+            Console.WriteLine($"Your available balance is: {balance.ToString("C")}");
             Console.WriteLine();
         }
 
-        static int withdrawMoney()
+        static decimal withdrawMoney(decimal balance, decimal value)
         {
-
+            return (balance - value);
         }
 
-        static int addMoney()
+        static decimal addMoney(decimal balance, decimal value)
         {
-
+            return (balance + value);
         }
     }
 }
